3e217471a01b83f8be91d6ada8d857a7ac739eb1
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-08-14 00:28:12 +0700

    question 4.2.1

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -33,10 +33,10 @@ event: Event
   border: 1px solid #39495c;
   margin-bottom: 10px;
 }
-.event-card:hover {
+/*.event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+}*/
 .event-link {
   text-decoration: none;
   color: #2c3e50;
