dab31c2304e5dc46c9b7d06bd18546293b264a70
MRSMIRROR <hunsawut_h@cmu.ac.th>
2024-08-14 01:57:53 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,13 @@ if (route.query.pageSize) {
 </script>
 
 <template>
+  <SpeedInsights/>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
+      <h1>Deploy with Vercel</h1>
       <div class="wrapper">
         <nav class="py-6">
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view', query: { pageSize: pageSize } }">Event</RouterLink> |
